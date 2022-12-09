31e54dee07598a72641f5c6e584eb2157483d3f8
Aniketh Nair <anikethnair.developer@gmail.com>
2022-12-09 14:46:07 +0530

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,51 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Name of libraries
+const VENDOR_LIBS = [
+  "react",
+  "lodash",
+  "redux",
+  "react-redux",
+  "react-dom",
+  "faker",
+  "react-input-range",
+  "redux-form",
+  "redux-thunk",
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: "./src/index.js",
+    vendor: VENDOR_LIBS, // This tells webpack that a separate bundle file needs to be generated
+  },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
